eb6ec2810c975f11a80024ca9140b5ab4a2af2b9
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2014-03-19 13:55:58 +0600

    del+add

--- dataaccess.js
+++ dataaccess.js
@@ -1,12 +1,9 @@
 var mpns = require('mpns');
 var config=require('./config.js');
 var utility=require('./utility.js');
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var monk = require('monk');
+
  var mailer= require('./mailsender.js');
- var parser=require('./parser.js');
- var mimelib = require("mimelib-noiconv");
+
 
 var debug = config.IS_DEBUG_MODE;
 
@@ -40,13 +37,13 @@ if(i<addresses.length){
                   Invitees.insert(entity,function(e,r){
                     if(e){
                        utility.log("insert Invitee error: " + e, 'ERROR');
-                       //connection.close();
+                       // 
                     }
                     else
                     {
                      mailer.sendMail(config.ATTENDEE_EMAIL_SUBJECT,config.ATTENDEE_EMAIL_BODY,result1.EmailID);
                      utility.log('Parsed Success email sent to '+result1.EmailID);
-                     //connection.close();
+                     // 
                      InsertMeetingInvitees(EmailAddresses,Invitees,invID,addresses,i+1,callback);
                    }
                   });
@@ -65,33 +62,33 @@ else{
   // body...
 }
 
-function InsertMeetingTolls(localtolls){
+function InsertMeetingTolls(connection,localtolls){
   
   if(localtolls==null) return;
   if(localtolls.length==0) return;
   utility.log("Meeting Tolls to insert");
   utility.log(localtolls);
-   mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
-    if(err) {
-      utility.log('database connection error: '+err,'ERROR');
+  if(connection==null) {
+      utility.log('database connection is null','ERROR');
+     
       return;
   }
       var Tolls = connection.collection('MeetingTolls');
       Tolls.insert(localtolls,function(err,rslt){
           if(err){
             utility.log('Insert MeetingTolls Error: '+err,'ERROR');
-            connection.close();
+             
           }
           else{
             utility.log("Successfully Inserted "+localtolls.length+" Meeting Tolls.");
-            connection.close();
+             
           }
       });
       
-  });
+
 }
 
-function insertInvitationEntity(entity,addresses,localtolls)
+function insertInvitationEntity(connection,entity,addresses,localtolls)
 {
    if(localtolls!=null && localtolls.length>0){
     for (var i = 0; i < localtolls.length; i++) {
@@ -99,10 +96,9 @@ function insertInvitationEntity(entity,addresses,localtolls)
     };
    }
 
-
-  mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
-   if(err) {
-      utility.log('database connection error: '+err,'ERROR');
+if(connection==null) {
+      utility.log('database connection is null','ERROR');
+     
       return;
   }
   var Invitations = connection.collection('Invitations');
@@ -127,7 +123,7 @@ function insertInvitationEntity(entity,addresses,localtolls)
     Invitations.findOne({"AccessCode": entity.AccessCode}, function(error, result_invite){
     if(error){
       utility.log("Error in find invitation with AccessCode to check duplicate" + error,'ERROR');
-       connection.close();
+        
     } else{
       //console.log("Invitation  found nor" + result_invite);
         if(result_invite == null){
@@ -135,15 +131,15 @@ function insertInvitationEntity(entity,addresses,localtolls)
           if(error)
           {
             utility.log("insertInvitationEntity() error: " + error, 'ERROR');
-            connection.close();
+             
           }
           else
           {
             utility.log('insert invitation result.........');
             utility.log(result);
             utility.log("Invitation inserted Successfully");
-            InsertMeetingInvitees(EmailAddresses,Invitees,result[0]._id,addresses,0,function(){ InsertMeetingTolls(localtolls);});
-            //connection.close();  
+            InsertMeetingInvitees(EmailAddresses,Invitees,result[0]._id,addresses,0,function(){ InsertMeetingTolls(connection,localtolls);});
+            //   
             
           }
         });
@@ -154,7 +150,7 @@ function insertInvitationEntity(entity,addresses,localtolls)
           if(error)
           {
             utility.log("update error in insertInvitationEntity() error: " + error, 'ERROR');
-            connection.close();
+             
           }
           else
           {
@@ -164,15 +160,15 @@ function insertInvitationEntity(entity,addresses,localtolls)
             Invitees.remove({Invitations_id:result_invite._id},function(err,res){
               if(err){
               utility.log("delete error in insertInvitationEntity() error: " + error, 'ERROR');
-              connection.close();
+               
               }
               else{
                 utility.log('deleted all previous invitees.')
-                 InsertMeetingInvitees(EmailAddresses,Invitees,result_invite._id,addresses,0,function(){ InsertMeetingTolls(localtolls);});
+                 InsertMeetingInvitees(EmailAddresses,Invitees,result_invite._id,addresses,0,function(){ InsertMeetingTolls(connection,localtolls);});
               }
             });
            
-            //connection.close();  
+            //   
             
           }
         });
@@ -187,94 +183,12 @@ function insertInvitationEntity(entity,addresses,localtolls)
  });
   
 
-  
-});
 
 }
 
 
 
-function insertInvitationEntity_backdated(entity,addresses)
-{
-  mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
-   if(err) {
-      utility.log('database connection error: '+err,'ERROR');
-      return;
-  }
-  var Invitations = connection.collection('Invitations');
-  var Invitees = connection.collection('Invitees');
-  var EmailAddresses = connection.collection('EmailAddresses');
 
-  
-
-  Invitations.findOne({"AccessCode": entity.AccessCode}, function(error, result_invite){
-    if(error){
-      utility.log("Error in find invitation with AccessCode to check duplicate" + error);
-    } else{
-      //console.log("Invitation  found nor" + result_invite);
-        if(result_invite == null){
-         Invitations.insert(entity, function(error, result) {
-          if(error)
-          {
-            utility.log("insertInvitationEntity() error: " + error, 'ERROR');
-            connection.close();
-          }
-          else
-          {
-            utility.log('insert invitation result.........||');
-            utility.log(result);
-            utility.log("Invitation inserted Successfully");
-            for (var i = 0; i < addresses.length; i++) {
-              //var emailID = addresses[i].address;
-              EmailAddresses.findOne({EmailID: addresses[i].address}, function(error, result1){
-                if(!error){
-                  if(result1==null){
-                    utility.log(addresses[i].address+' not found in white list');
-                      //send email
-                     
-                    mailer.sendMail(config.NOT_WHITELISTED_EMAIL_SUBJECT,config.NOT_WHITELISTED_EMAIL_BODY,addresses[i].address);
-                  
-                  }
-                  else{
-                    //var userID = result1.UserID;
-                    var entity = {
-                    "UserID": result1.UserID,
-                    "EmailID": result1.EmailID,
-                    "Invitations_id": result[0]._id
-                  };
-                   utility.log('invitee object to insert');
-                   utility.log(entity);
-                  Invitees.insert(entity,function(e,r){
-                    if(e){
-                       utility.log("insert Invitee error: " + e, 'ERROR');
-                       //connection.close();
-                    }
-                    else
-                    {
-                     mailer.sendMail(config.ATTENDEE_EMAIL_SUBJECT,config.ATTENDEE_EMAIL_BODY,result1.EmailID);
-                     //connection.close();
-                   }
-                  });
-                 
-                    
-                  }
-                  
-                }
-              });
-            }
-            //connection.close();  
-            
-          }
-        });
-      }
-      else{
-        utility.log("Invitation already exist for AccessCode: "+entity.AccessCode);
-      }
-    }
-  });
-});
-
-}
 
 
 
@@ -288,13 +202,13 @@ function minutesDiff(start, end){
 }
 
 /// Method to send/push notification to MPNS
-function PushNotification(notificationRemainderTime)
+function PushNotification(connection,notificationRemainderTime)
 {
 
 
-  mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
-   if(err) {
-      utility.log('database connection error: '+err,'ERROR');
+  if(connection==null) {
+      utility.log('database connection is null','ERROR');
+     
       return;
   }
 
@@ -318,7 +232,7 @@ function PushNotification(notificationRemainderTime)
     if(error)
     {
       utility.log("find Invitations error: " + error, 'ERROR');
-      connection.close();
+       
     }
     else
     {
@@ -339,7 +253,7 @@ function PushNotification(notificationRemainderTime)
             if(error)
             {
               utility.log("find Invitees error: " + error, 'ERROR');
-              connection.close();
+               
             }
             else
             {
@@ -357,7 +271,7 @@ function PushNotification(notificationRemainderTime)
                   if(error)
                   {
                     utility.log("find registration error: " + error, 'ERROR');
-                    connection.close();
+                     
                   }
                   else
                   {
@@ -374,10 +288,14 @@ function PushNotification(notificationRemainderTime)
 
                         //console.log(inv);
                       var RemainderMinute = registrations.RemainderMinute;
+                       var md = minutesDiff( inv.InvTime,new Date());
+                      if(md<=50){
                       utility.log("Remainder Time for "+att.UserID +" is "+RemainderMinute+" minutes");
-                      var md = minutesDiff( inv.InvTime,new Date());
+                     
                       utility.log("meeting "+inv.Subject+" of "+att.UserID+" remaining minute: "+md);
                       
+                      
+
                       if(md <= RemainderMinute && RemainderMinute >-1 ){
                         //pushInfo["PushUrl"] = registrations.Handle;
                         var tileObj = {
@@ -388,7 +306,9 @@ function PushNotification(notificationRemainderTime)
                                   };
                         mpns.sendTile(registrations.Handle, tileObj, function(){utility.log('Pushed to ' + att.UserID+" for "+inv.Subject);});
                       }
-                      //connection.close();
+                    }
+                    
+                      // 
                     } 
                     // else {
                     //   pushInfo["PushUrl"] =null;
@@ -409,240 +329,14 @@ function PushNotification(notificationRemainderTime)
         // response.end();
       }
     });
-});
-}
-
-
-function PushNotification_bk(notificationRemainderTime)
-{
-
-
-  mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
-   if(err) {
-      utility.log('database connection error: '+err,'ERROR');
-      return;
-  }
-
-  var Invitations = connection.collection('Invitations');
-  var Invitees = connection.collection('Invitees');
-  var Registrations = connection.collection('Registrations');
-  
-  var sttime =  addMinutes(new Date(), 0);
-  //console.log(sttime);
-  // var edtime = addMinutes(new Date(), notificationRemainderTime/(1000*60));
-  var edtime = addMinutes(new Date(), (24*60));
-  //console.log(edtime);
-  var invtime = {
-    InvTime: {
-      $gte: sttime,
-      $lte: edtime
-    }
-  }
- 
-  Invitations.find(invtime).toArray( function(error, invites) {
-    if(error)
-    {
-      utility.log("find Invitations error: " + error, 'ERROR');
-      connection.close();
-    }
-    else
-    {
-      if(debug==true)
-      {
-      utility.log("eligible invitations for push");
-      utility.log(invites);
-      }
-      var pushInfo = [];
-      for (var i = 0; i < invites.length; i++) {
-         
-        pushInfo["Subject"] = invites[i].Subject;
-        pushInfo["Agenda"] = invites[i].Agenda;
-        pushInfo["InvTime"] = invites[i].InvTime;
-
-          // Invitations_ids.push(invites[i]._id);
-          Invitees.find({Invitations_id: invites[i]._id}).toArray( function(error, invitees) {
-            if(error)
-            {
-              utility.log("find Invitees error: " + error, 'ERROR');
-              connection.close();
-            }
-            else
-            {
-              if(debug==true)
-              {
-              utility.log("eligible invitees for push");
-              utility.log(invitees);
-              }
-
-              for (var j = 0; j < invitees.length; j++) {
-                
-                pushInfo["UserID"] = invitees[j].UserID;
-
-                Registrations.findOne({UserID: invitees[j].UserID.trim()}, function(error, registrations) {
-                  if(error)
-                  {
-                    utility.log("find registration error: " + error, 'ERROR');
-                    connection.close();
-                  }
-                  else
-                  {
-                    if(debug==true)
-                    {
-                    utility.log('Invitees Push URL Info' );
-                    utility.log(registrations);
-                    }
-                    // console.log("Inv ID: "+invites[i]._id);
-                    // console.log(invitees[j]);
-                    // console.log(registrations); RemainderMinute
-                    if(registrations != null)
-                    {
-
-                        utility.log(invitees[j]);
-                      var RemainderMinute = registrations.RemainderMinute;
-                      utility.log("Remainder Time for "+invitees[j].UserID +" is "+RemainderMinute+" minutes");
-                      var md = minutesDiff( invites[i].InvTime,new Date());
-                      utility.log("meeting "+invites[i].Subject+" of "+invitees[j].UserID+" remaining minute: "+md);
-                      
-                      if(md <= RemainderMinute){
-                        pushInfo["PushUrl"] = registrations.Handle;
-                        var tileObj = {
-                                  'title': invites[i].Subject,
-                                  'backTitle': "Next Conference",
-                                  'backBackgroundImage': "/Assets/Tiles/BackTileBackground.png",
-                                  'backContent': invites[i].Agenda+"("+md+" minutes remaining)"
-                                  };
-                        mpns.sendTile(registrations.Handle, tileObj, function(){utility.log('Pushed to ' + invitees[j].UserID);});
-                      }
-                      //connection.close();
-                    } 
-                    // else {
-                    //   pushInfo["PushUrl"] =null;
-                    //   utility.log("Can't find push URL for "+pushInfo["UserID"]+" . so can't push notification.",'WARNING');
-                    // }
-                    // console.log(pushInfo);
-
-                  }
-                });
-              }
-            }
-          });
-          
-        }
-        //return JSON.stringify(result);
-        // response.setHeader("content-type", "text/plain";
-        // response.write("{\"Tolls\":" + JSON.stringify(result.Toll) + "}";
-        // response.end();
-      }
-    });
-});
-}
-
-
-
-
-
-function PushNotification_back(notificationRemainderTime)
-{
-
-mongo.MongoClient.connect(config.MONGO_CONNECTION_STRING, function(err, connection) {
- if(err) {
-      utility.log('database connection error: '+err,'ERROR');
-      return;
-  }
-
-  var Invitations = connection.collection('Invitations');
-  var Invitees = connection.collection('Invitees');
-  var Registrations = connection.collection('Registrations');
-  var sttime = new Date(); //addMinutes(new Date(), -99999999);
-  //console.log(sttime);
-  var edtime = addMinutes(new Date(), notificationRemainderTime/(1000*60));
-  //console.log(edtime);
-  var invtime = {
-    InvTime: {
-      $gte: sttime,
-      $lte: edtime
-    }
-  }
-
-  Invitations.find(invtime).toArray( function(error, invites) {
-    if(error)
-    {
-      utility.log("find Invitations error: " + error, 'ERROR');
-      connection.close();
-    }
-    else
-    {
-
-      var pushInfo = [];
-      for (var i = 0; i < invites.length; i++) {
-         
-        pushInfo["Subject"] = invites[i].Subject;
-        pushInfo["Agenda"] = invites[i].Agenda;
-
-          // Invitations_ids.push(invites[i]._id);
-          Invitees.find({Invitations_id: invites[i]._id}).toArray( function(error, invitees) {
-            if(error)
-            {
-              utility.log("find Invitees error: " + error, 'ERROR');
-              connection.close();
-            }
-            else
-            {
-              
-              for (var j = 0; j < invitees.length; j++) {
-                
-                pushInfo["UserID"] = invitees[j].UserID;
-
-                Registrations.findOne({UserID: invitees[j].UserID.trim()}, function(error, registrations) {
-                  if(error)
-                  {
-                    utility.log("find registration error: " + error, 'ERROR');
-                    connection.close();
-                  }
-                  else
-                  {
-                    // console.log("Inv ID: "+invites[i]._id);
-                    // console.log(invitees[j]);
-                    // console.log(registrations);
-                    if(registrations != null)
-                    {
-                      pushInfo["PushUrl"] = registrations.Handle;
-                      var tileObj = {
-                                'title': pushInfo["Subject"],
-                                'backTitle': "Next Conference",
-                                'backBackgroundImage': "/Assets/Tiles/BackTileBackground.png",
-                                'backContent': pushInfo["Agenda"]
-                                };
-                    mpns.sendTile(pushInfo["PushUrl"], tileObj, function(){utility.log('Pushed to ' + pushInfo["UserID"]);});
-                    connection.close();
-                    } 
-                    // else {
-                    //   pushInfo["PushUrl"] =null;
-                    //   utility.log("Can't find push URL for "+pushInfo["UserID"]+" . so can't push notification.",'WARNING');
-                    // }
-                    // console.log(pushInfo);
-
-                  }
-                });
-              }
-            }
-          });
-          
-        }
-        
-        //return JSON.stringify(result);
-        // response.setHeader("content-type", "text/plain");
-        // response.write("{\"Tolls\":" + JSON.stringify(result.Toll) + "}");
-        // response.end();
-      }
-
-    });
-
-
-
-});
 
 }
+
+
+
+
+
+
 
 
 
