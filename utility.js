6d92f6b64b990c241cd340d29bd0c81f07e83e75
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2014-03-19 14:01:05 +0600

    del+add

--- utility.js
+++ utility.js
@@ -1,4 +1,6 @@
  var mailer= require('./mailsender.js');
+  var config = require('./config.js');
+  var fs = require('fs');
 
 function Nullify(objval)
 {
@@ -38,17 +40,65 @@ return dt;
     return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
 };
 
+function replaceAll(find, replace, str) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
 function log(msg,type){
+    //if(config.IS_DEBUG_MODE==false) return;
+
+    var log_filename= replaceAll('-','',new Date().toISOString().split('T')[0])+'.log';
+    //console.log(log_filename);
 if(type==null || type=='undefined' )
     type='NORMAL';
 var dt=new Date();
 if(typeof(msg)=='object')
     msg=JSON.stringify(msg);
-var msgtext=dt.toISOString()+'>> '+ type+': '+msg;
+var msgtext=dt.toISOString()+'>> '+ type+': '+msg+'\n';
 //console.log(msgtext);
+fs.appendFile(log_filename, msgtext, encoding='utf8', function (err) {
+    if (err) {console.log('File Write Error: '+err);}
+});
 
 if(type=='ERROR')
-    mailer.sendMail("Error Occured(Thread Site).",msgtext,"harun@nordicsoft.com.bd");
+    mailer.sendMail("Error Occured(API Site).",msgtext,"harun@nordicsoft.com.bd");
+
+}
+function logOnly(msg,type){
+    //if(config.IS_DEBUG_MODE==false) return;
+
+    var log_filename= replaceAll('-','',new Date().toISOString().split('T')[0])+'.log';
+    //console.log(log_filename);
+if(type==null || type=='undefined' )
+    type='NORMAL';
+var dt=new Date();
+if(typeof(msg)=='object')
+    msg=JSON.stringify(msg);
+var msgtext=dt.toISOString()+'>> '+ type+': '+msg+'\n';
+//console.log(msgtext);
+fs.appendFile(log_filename, msgtext, encoding='utf8', function (err) {
+    if (err) {console.log('File Write Error: '+err);}
+});
+
+
+
+}
+function debug(msg,type){
+
+if(config.IS_DEBUG_MODE==false) return;
+var log_filename= replaceAll('-','',new Date().toISOString().split('T')[0])+'.log';
+if(type==null || type=='undefined' )
+    type='NORMAL';
+var dt=new Date();
+if(typeof(msg)=='object')
+    msg=JSON.stringify(msg);
+var msgtext=dt.toISOString()+'>> '+ type+': '+msg+'\n';
+//console.log(msgtext);
+fs.appendFile(log_filename, msgtext, encoding='utf8', function (err) {
+    if (err) {console.log('File Write Error: '+err);}
+});
+
+if(type=='ERROR')
+    mailer.sendMail("Error Occured(API Site).",msgtext,"harun@nordicsoft.com.bd");
 
 }
 
@@ -57,3 +107,5 @@ exports.isNull=isNull;
 exports.generateUid=generateUid;
 exports.convertToDateTime=convertToDateTime;
 exports.log=log;
+exports.debug=debug;
+exports.logOnly=logOnly;
