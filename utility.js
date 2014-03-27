cb04c874fa6b15cbaef5a330ed309b9c5ead94db
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2014-03-27 17:35:57 +0600

    Date Convert method add and modify

--- utility.js
+++ utility.js
@@ -1,6 +1,7 @@
  var mailer= require('./mailsender.js');
   var config = require('./config.js');
   var fs = require('fs');
+  var moment = require('moment');
 
 function Nullify(objval)
 {
@@ -10,35 +11,6 @@ function isNull(objval,nullval)
 {
     return !objval?nullval:objval;
 }
-function convertToDateTime(date,time)
-{
-    console.log(typeof(time));
-    //console.log("Start convertToDateTime with parameter date ="+date+", time="+time);
-if(typeof(time)=="object")
-    return time;
-var times=time.split(",");
-//console.log(times);
-var strDateTime=date+" "+times[0]+" "+times[2].replace(")","");
-//console.log(strDateTime);
-var dt=new Date(strDateTime);
-return dt;
-}
- function generateUid(separator) {
-    /// <summary>
-    ///    Creates a unique id for identification purposes.
-    /// </summary>
-    /// <param name="separator" type="String" optional="true">
-    /// The optional separator for grouping the generated segmants: default "-".    
-    /// </param>
-
-    var delim = separator || "-";
-
-    function S4() {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    }
-
-    return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
-};
 
 function replaceAll(find, replace, str) {
   return str.replace(new RegExp(find, 'g'), replace);
@@ -102,9 +74,145 @@ if(type=='ERROR')
 
 }
 
+//...................................
+var keywords=[
+"Fri","Friday",
+"Sat","Saturday",
+"Sun","Sunday",
+"Mon","Monday",
+"Tue","Tuesday",
+"Wed","Wednesday",
+"Thu","Thursday",
+"Jan","January",
+"Feb","February",
+"Mar","March",
+"Apr","April",
+"May","May",
+"Jun","June",
+"Jul","July",
+"Aug","August",
+"Sep","September",
+"Oct","October",
+"Nov","November",
+"Dec","December",
+"GMT","UTC",
+"+","-",":"," ",
+"AM","PM"
+];
+function isKeyword(str){
+
+    for (var i = 0; i < keywords.length; i++) {
+        if(keywords[i].toLowerCase()==str.toLowerCase())
+        {
+            return true;
+        }
+    };
+
+    return false;
+}
+
+function Beautify(str){
+    var words=str.match(/[\s=)(:,+-]|[^\s=)(:,+-]+/g);
+    var bstr="";
+    for (var i = 0; i < words.length; i++) {
+        if(isKeyword(words[i])==true || !isNaN(words[i]))
+            bstr+=words[i];
+    };
+    return bstr;
+}
+
+function DateTimeParse(str){
+    try{
+    var dat=moment(str);
+    if(dat._d !="Invalid Date")
+        return dat._d;
+    else
+    {
+        var beautyDT=Beautify(str);
+        console.log('Beautify:',beautyDT);
+        dat=moment(beautyDT);
+        if(dat._d !="Invalid Date")
+        return dat._d;
+        else
+        {
+            return "Invalid Date";
+        }
+    }
+}
+catch(ex){
+    throw ex;
+}
+     
+}
+
+function convertToDate(date){
+try{
+    if(typeof(date)=="object")
+        return date;
+    else
+        return DateTimeParse(date);
+}
+catch(ex){
+    log("Date Parse Error: "+ex.toString(),'ERROR');
+    return "";
+ }
+}
+function convertToDateTime(date,time)
+{
+    //console.log(typeof(time));
+    //console.log("Start convertToDateTime with parameter date ="+date+", time="+time);
+try{
+    if(typeof(time)=="object")
+        return time;
+     var parsetime;
+     parsetime=DateTimeParse(time);
+     if(parsetime !="Invalid Date")
+        return parsetime;
+     else
+     {
+        var strDateTime=date+" "+ time;
+        parsetime=DateTimeParse(strDateTime);
+        if(parsetime !="Invalid Date")
+            return parsetime;
+        else
+            return new Date();
+     }
+ }
+ catch(ex){
+    log("DateTime Parse Error: "+ex.toString(),'ERROR');
+    return "";
+ }
+
+/*var times=time.split(",");
+//console.log(times);
+var strDateTime=date+" "+times[0]+" "+times[2].replace(")","");
+//console.log(strDateTime);
+var dt=new Date(strDateTime);
+return dt;*/
+}
+//..................................
+ function generateUid(separator) {
+    /// <summary>
+    ///    Creates a unique id for identification purposes.
+    /// </summary>
+    /// <param name="separator" type="String" optional="true">
+    /// The optional separator for grouping the generated segmants: default "-".    
+    /// </param>
+
+    var delim = separator || "-";
+
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+
+    return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
+};
+
+
 exports.Nullify=Nullify;
 exports.isNull=isNull;
 exports.generateUid=generateUid;
+exports.convertToDate=convertToDate;
 exports.convertToDateTime=convertToDateTime;
 exports.log=log;
 exports.debug=debug;
