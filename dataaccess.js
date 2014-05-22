8324661917c83c31416cd416fade8cf5ccbd711e
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2014-05-22 16:36:51 +0600

    Fix tile notification time.

--- dataaccess.js
+++ dataaccess.js
@@ -477,11 +477,12 @@ function PushNotification(connection, notificationRemainderTime)
                                 //   'backContent' : inv.Subject + '\n' + moment(inv.InvTime).format('hh:mm A')  //inv.Agenda+"("+md+" minutes remaining)"
                                 // };
                                 var backHeader = moment(inv.InvTime).date() == moment().date() ? 'TODAY ' : 'TOMORROW ';
+                                var meetingTime = moment(inv.InvTime.toISOString()).format('hh:mm A');
                                 var tileObj = {
                                   'title' : '', // inv.Subject,
                                   'backTitle' : 'Telvoy', // "Next Conference",
                                   'backBackgroundImage' : "/Assets/Tiles/BackTileBackground.png",
-                                  'backContent' : backHeader + inv.Subject + '\n' + moment(inv.InvTime.toISOString()).format('hh:mm A')  //inv.Agenda+"("+md+" minutes remaining)"
+                                  'backContent' : backHeader + '\n' + inv.Subject + meetingTime  //inv.Agenda+"("+md+" minutes remaining)"
                                 };
                                 mpns.sendTile(registrations.Handle, tileObj, function(){
                                   utility.log('Pushed to ' + te + " for " + inv.Subject);
